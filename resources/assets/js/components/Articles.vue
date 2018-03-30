52a18ffad330aa869138ba6a95a4a345484a21d5
dererumnatura <pollindrom@gmail.com>
2018-03-30 13:56:28 +0300

    finished

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "articles.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "navbar.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
